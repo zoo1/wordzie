752a3d2843de49146aa73991b62a55e253724e35
Zachary Ovington <zoo1@pitt.edu>
2015-08-10 21:03:57 -0400

    init script etc

---
+++ mysqlinit.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+var config = require("./config");
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : config.msqluser,
+  password : config.msqlpass,
+  database : 'wordzie'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }});
+
+connection.query("DROP TABLE words", function(err) { });
+connection.query("DROP TABLE lists", function(err) { });
+connection.query("DROP TABLE stats", function(err) { });
+
+connection.query("CREATE TABLE words (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, word VARCHAR(30) NOT NULL, definitions VARCHAR(500) NOT NULL, sentence VARCHAR(200))" 
+,function(err) { });
+connection.query("CREATE TABLE lists ( listid INT UNSIGNED, wordid INT NOT NULL)" 
+,function(err) { });
+connection.query("CREATE TABLE stats ( stat VARCHAR(30), value VARCHAR(30))" 
+,function(err) { });
+
+
+connection.query("INSERT INTO stats values ('lastdate', '1970-11-11')",function(err) { }); //insert old date so new word will be fetched
+connection.query("INSERT INTO stats values ('wordofday', '')",function(err) { });
+connection.end();
