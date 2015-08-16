d72a6ae74ea40bf74846e550e67e06f903bb9fc6
Zachary Ovington <zoo1@pitt.edu>
2015-08-16 10:44:30 -0400

    made more bootstappy/angulary

--- public/js/directives.js
+++ public/js/directives.js
@@ -10,24 +10,24 @@ app.directive('onKeyupFn', ['$http', function($http) {
         var keyupFn = scope.$eval(attrs.onKeyupFn);
         elm.bind('keyup', function(evt) {
           var word = elm.context.value;
-            elm.context.style.background = '#FFFFFF';
-            if(/[^\s]/.test(word) && word != scope.lookup) //if word is whitespace or same as before
-            {
-              setTimeout(function(){
-                if(word === elm.context.value)
-                {
-                  $http.get('/api/word/'+word).
-                  then(function(response) {
-                    var data = response.data
-                    if(data.error)
-                      elm.context.style.background = '#FF9494';
-                    else
-                    {
-                      scope.pre = "";
-                      scope.lookup = word;
-                      scope.lookuparr = data;
-                    }
-                  }, function(response) {
+          elm.removeClass("alert-danger");
+          if(/[^\s]/.test(word) && word != scope.lookup) //if word is whitespace or same as before
+          {
+            setTimeout(function(){
+              if(word === elm.context.value)
+              {
+                $http.get('/api/word/'+word).
+                then(function(response) {
+                  var data = response.data
+                  if(data.error)
+                    elm.addClass("alert-danger");
+                  else
+                  {
+                    scope.pre = "";
+                    scope.lookup = word;
+                    scope.lookuparr = data;
+                  }
+                }, function(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
